cb4950a2aec5e0d2472a347f867a6c07559f9fd4
yoni-rlev <yonih@r-lev.com>
2026-04-09 01:11:17 +0300

    1.3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const fs = require('fs');
-
+ 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const DB_FILE = './database.json';
